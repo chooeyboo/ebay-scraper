87d24751a3a755be7d34be4e4d0292cfa72d6db6
Chris Wang <2005chriswang@gmail.com>
2024-02-20 10:29:16 -0600

    package tracking too hard doing ebay scraping instead

---
+++ EBay.py
@@ -0,0 +1,35 @@
+from selectorlib import Extractor
+import json 
+import requests 
+
+eBay = Extractor.from_yaml_file('EBay.yml')
+
+def scraper(url):  
+
+    headers = {
+        'dnt': '1',
+        'upgrade-insecure-requests': '1',
+        'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.61 Safari/537.36',
+        'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+        'sec-fetch-site': 'same-origin',
+        'sec-fetch-mode': 'navigate',
+        'sec-fetch-user': '?1',
+        'sec-fetch-dest': 'document',
+        'referer': 'https://www.ebay.com/',
+        'accept-language': 'en-GB,en-US;q=0.9,en;q=0.8',
+    }
+
+    print("Downloading %s"%url)
+    req = requests.get(url, headers=headers)
+    if req.status_code > 500:
+        print("Page was blocked by eBay. :(")
+        return None
+    return eBay.extract(req.text)
+
+with open("urls.txt",'r') as urls, open('result.jsonl','w') as outfile:
+    for url in urls.read().splitlines():
+        data = scraper(url) 
+        if data:
+            json.dump(data,outfile)
+            outfile.write("\n")
+                
--- README.md
+++ README.md
@@ -1,13 +1,1 @@
-# Package Tracker
-
-This website allows you to check the status and estimated delivery date of packages based on their tracking numbers. The website currently supports tracking numbers from USPS, UPS, and FedEx.
-
-Group: Minasha Gunarathna and Chris Wang
-
-## How to Run
-
-Dependencies:
-Node.js with the following packages:
-express, body-parser, node-fetch, cheerio, esm
-
-Open a terminal in the directory of the downloaded files and run `npm start`.
+hi
